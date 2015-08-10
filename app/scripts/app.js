08552314c20e93a97d5012d8e262bae49a0da02d
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2015-08-10 14:37:30 +0200

    Read example RDF file and display all triples found
    An example rdf file was added. Which is read using N3 and all triples in
    the file are displayed in the index.html.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,6 +19,24 @@
       'ui.bootstrap'
     ])
     .run(function() {
+    })
+    .controller('rdfCtrl', function ($scope, $http){
+      var parser = N3.Parser();
+
+      $scope.triples = [];
+      $scope.test = 0;
+
+      $http.get('data/4M1J-3MC0-TWKJ-V1W8.xml.perspective.trig')
+        .then(function (response){
+          parser.parse(response.data, function (error, triple, prefixes) {
+            // See https://github.com/RubenVerborgh/N3.js/ for details on how
+            // the function on the parsed data is called.
+            if (triple){
+              $scope.triples.push(triple);
+            }
+            $scope.$apply();
+          });
+        });
     });
 
   angular.module('uncertApp.templates', []);
