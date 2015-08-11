b58cac79acf7fd22c78222e87a10066943e2f8f0
maartenvm <m.vanmeersbergen@esciencecenter.nl>
2015-08-11 16:28:51 +0200

    modularized rdf stuff by Janneke

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,11 @@
 // The app
+/* global N3:false */
 
 (function() {
   'use strict';
+
+  angular.module('uncertApp.n3', [])
+    .constant('N3', N3);
 
   /**
    * @ngdoc overview
@@ -20,33 +24,18 @@
       'ngCookies',
       'ui.bootstrap',
       'uncertApp.authentication',
+      'uncertApp.rdf',
       //'uncertApp.sparql',
       'uncertApp.text'
     ])
     .run(function() {
-    })
-    .controller('rdfCtrl', function ($scope, $http){
-      var parser = N3.Parser();
-
-      $scope.triples = [];
-      $scope.test = 0;
-
-      $http.get('data/4M1J-3MC0-TWKJ-V1W8.xml.perspective.trig')
-        .then(function (response){
-          parser.parse(response.data, function (error, triple, prefixes) {
-            // See https://github.com/RubenVerborgh/N3.js/ for details on how
-            // the function on the parsed data is called.
-            if (triple){
-              $scope.triples.push(triple);
-            }
-            $scope.$apply();
-          });
-        });
     });
+
 
   angular.module('uncertApp.templates', []);
   angular.module('uncertApp.utils', ['uncertApp.templates']);
   angular.module('uncertApp.authentication', ['ngCookies']);
   angular.module('uncertApp.sparql', ['uncertApp.utils','uncertApp.authentication']);
   angular.module('uncertApp.text', ['uncertApp.sparql', 'uncertApp.utils']);
+  angular.module('uncertApp.rdf', ['uncertApp.n3']);
 })();
