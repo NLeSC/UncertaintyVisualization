697093c3bfcd9bb8d3335c49e45e33ebbbb45235
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2015-08-12 11:30:43 +0200

    Do sparql request in flask app
    The flask app returns results from the knowledge store for a fixed
    example query. It uses the username and password from the authorization
    service.

--- app/scripts/sparql/sparql.service.js
+++ app/scripts/sparql/sparql.service.js
@@ -2,7 +2,7 @@
   'use strict';
 
   function SparqlService($q, $http, Messagebus, AuthenticationService) {
-    this.knowledgeStoreURL = 'https://knowledgestore2.fbk.eu/nwr/dutchhouse/sparql?';
+    this.knowledgeStoreURL = 'http://localhost:5000/?';
     //this.knowledgeStoreURL = 'http://httpbin.org/basic-auth/user/passwd';
 
     this.exampleQuery = 'query=SELECT%20*%20WHERE%20%7Bdbpedia%3ABarack_Obama%20rdfs%3Alabel%20%3Flabel%20.%20%7D%20LIMIT%20100';
@@ -14,9 +14,8 @@
     this.initialized = false;
 
     this.init = function() {
-      debugger
       this.doQuery().then(function success(queryResult) {
-        debugger
+        console.log(queryResult);
       });
     };
 
--- server.py
+++ server.py
@@ -1,11 +1,23 @@
 from flask import Flask
+from SPARQLWrapper import SPARQLWrapper, JSON
+from flask import request
+from flask.ext.cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    auth = request.authorization
+    sparql = SPARQLWrapper('https://knowledgestore2.fbk.eu/nwr/dutchhouse/sparql')
+    sparql.setQuery("""
+        SELECT * WHERE {dbpedia:Barack_Obama rdfs:label ?label . } LIMIT 100
+        """)
+    sparql.setCredentials(auth.username, auth.password)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    r = ['{} {}'.format(k, v) for k, v in results.iteritems()]
+    return ' '.join(r)
 
 if __name__ == '__main__':
-
-    app.run()
+    app.run(debug=True)
