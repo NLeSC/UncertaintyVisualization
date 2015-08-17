f206b5d1e06c79376587097512fc89c0ebb42ee7
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2015-08-17 14:36:32 +0200

    Fix tests for formatSparqlBindingFilter

--- test/spec/sparql/sparql.filters.spec.js
+++ test/spec/sparql/sparql.filters.spec.js
@@ -5,25 +5,30 @@ describe('sparql filters', function() {
   beforeEach(module('uncertApp.templates'));
   beforeEach(module('uncertApp.sparql'));
 
-  describe("FormatSparqlBinding", function() {
-    var formatSparqlBinding;
+  describe('FormatSparqlBinding', function() {
+    var formatSparqlBindingFilter;
 
     beforeEach(inject(function(_formatSparqlBindingFilter_) {
       formatSparqlBindingFilter = _formatSparqlBindingFilter_;
+    }));
 
-      it('returns the value when type=literal', function() {
-        var value = 'some_string'
-        var input = {'type': 'literal', 'value': value};
-        expect(formatSparqlBindingFilter(input)).toEqual(value);
-      });
+    it('returns the value when type=literal', function() {
+      var value = 'some_string';
+      var input = {
+        'type': 'literal',
+        'value': value
+      };
+      expect(formatSparqlBindingFilter(input)).toEqual(value);
+    });
 
-      it('returns a link to the value when type=uri', function() {
-        var value = 'http://www.newsreader-project.eu/provenance/author/Christine_Tierney_The_Detroit_News'
-        var input = {'type': 'uri', 'value': value};
-        var url = '<a href="'+input.value+'">'+input.value+'</a>'
-        expect(formatSparqlBindingFilter(input)).toEqual(url);
-      });
-
+    it('returns a link to the value when type=uri', inject(function($sce, formatSparqlBindingFilter) {
+      var value = 'http://www.newsreader-project.eu/provenance/author/Christine_Tierney_The_Detroit_News';
+      var input = {
+        'type': 'uri',
+        'value': value
+      };
+      var url = '<a href="' + input.value + '">' + input.value + '</a>';
+      expect(formatSparqlBindingFilter(input).$$unwrapTrustedValue()).toEqual(url);
     }));
   });
 });
