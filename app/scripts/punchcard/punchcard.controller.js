225869b064b3706d8d42ae735b1d7d86ecef69a2
Maartenvm <m.vanmeersbergen@esciencecenter.nl>
2015-11-25 11:00:08 +0100

    Bubble group chart re-used for actors.

--- app/scripts/punchcard/punchcard.controller.js
+++ app/scripts/punchcard/punchcard.controller.js
@@ -90,6 +90,250 @@
 
 
           //The row Chart which shows how important the groups are in terms of climax scores
+          var subwayChart = dc.subwayChart('#subwayChart');
+
+          //The dimension for the subwayChart. We use time for x and group for y,
+          //and bin everything in the same group number and day.
+
+          //Dimension of the list of unique actors present in each event.
+          var subwayActorsDimension = ndx.dimension(function(d) {
+            var time = d3.time.format('%Y%m%d').parse(d.time);
+            var concatenatedActors = [];
+
+            var keys = Object.keys(d.actors);
+            keys.forEach(function (key) {
+              var keysActors = d.actors[key];
+              keysActors.forEach(function(keysActor) {
+                concatenatedActors.push(keysActor);
+              });
+            });
+            var uniqueActors = arrayUnique(concatenatedActors);
+
+            return [uniqueActors, time];
+          });
+
+          //The group for the subwayGroup.
+          var subwayGroup = subwayActorsDimension.group().reduce(
+            //Add something to our temporary collection
+            function(p, v) {
+              //Climax score summed for all events with the same time(day) and group(number).
+              p.climax = p.climax + v.climax;
+
+              var keys = Object.keys(v.actors);
+              keys.forEach(function (key) {
+                var keysActors = v.actors[key];
+                keysActors.forEach(function(keysActor) {
+                  var actorLabel = key + ' : ' + keysActor;
+                  p.actors[actorLabel] = (p.actors[actorLabel] || 0) + v.climax;
+                });
+              });
+
+              //Sum event values over all events fitting this time and group.
+              p.events[v.event] = (p.events[v.event] || 0) + v.climax;
+
+              //Sum label values over all events fitting this time and group.
+              v.labels.forEach(function(l) {
+                p.labels[l] = (p.labels[l] || 0) + v.climax;
+              });
+
+              return p;
+            },
+            //Remove something from our temporary collection, (basically do
+            //everything in the add step, but then in reverse).
+            function(p, v) {
+              p.climax = p.climax - v.climax;
+
+              var keys = Object.keys(v.actors);
+              keys.forEach(function (key) {
+                var keysActors = v.actors[key];
+                keysActors.forEach(function(keysActor) {
+                  var actorLabel = key + ' : ' + keysActor;
+                  p.actors[actorLabel] = (p.actors[actorLabel] || 0) - v.climax;
+                });
+              });
+
+              p.events[v.event] = (p.events[v.event] || 0) - v.climax;
+
+              v.labels.forEach(function(l) {
+                p.labels[l] = (p.labels[l] || 0) - v.climax;
+              });
+
+              return p;
+            },
+            //Set up the inital data structure.
+            function() {
+              return {climax: 0, events: {}, actors: {}, labels: {}};
+            }
+          );
+
+          var ordinalGroupScale2;
+
+          //Set up the
+          subwayChart
+            //Sizes in pixels
+            .width(parseInt(d3.select('#laneChart').style('width'), 10))
+            .height(400)
+            .margins({
+              top: 10,
+              right: 0,
+              bottom: 20,
+              left: 100
+            })
+
+            //Bind data
+            .dimension(subwayActorsDimension)
+            .group(subwayGroup)
+
+            //The time this chart takes to do its animations.
+            .transitionDuration(1500)
+
+            //x Axis
+            // .xAxisLabel('time')
+            .x(d3.time.scale())
+            .elasticX(true)
+            .xAxisPadding(100)
+            .keyAccessor(function(p) {
+              //The time of this event
+              return p.key[1];
+            })
+
+            //y Axis
+            // .yAxisLabel('group')
+            .y(ordinalGroupScale = d3.scale.ordinal()
+            .domain((function() {
+              //Because we use an ordinal scale here, we have to tell the chart
+              //which values to expect.
+              var domain = subwayGroup.all().map(function(d) {
+                //The group of this event
+                if (d.value.climax > 0) {
+                  return(d.key[0]);                  
+                }
+              });
+              return domain;
+            })()).copy()
+            )
+            .valueAccessor(function(p) {
+              //The group of this event
+              return p.key[0];
+            })
+
+            //Radius of the bubble
+            .r(d3.scale.linear().domain(
+              //We assume a climax value in the domain [ 0 , n ]
+              [0, d3.max(subwayChart.data(), function (e) {
+                    return e.value.climax;
+                  })]))
+            .radiusValueAccessor(function(p) {
+              return p.value.climax;
+            })
+            .minRadius(2)
+            .maxBubbleRelativeSize(0.015)
+
+            //Everything related to coloring the bubbles
+            .colors(colorbrewer.RdYlGn[9])
+            // We currently color the bubble using the same values as the radius
+            .colorDomain(
+              [0, d3.max(subwayChart.data(), function (e) {
+                return e.value.climax;
+              })])
+            .colorAccessor(function(d) {
+              return d.value.climax;
+            })
+
+            //Labels printed just above the bubbles
+            .renderLabel(true)
+            .minRadiusWithLabel(0)
+            .label(function(p) {
+              var mostImportantLabel;
+              var climaxScoreOfMostImportantLabel = -1;
+              //Get the most important label (highest climax score)
+              var labels = Object.keys(p.value.labels);
+              labels.forEach(function(l) {
+                if (p.value.labels[l] > climaxScoreOfMostImportantLabel) {
+                  mostImportantLabel = l;
+                  climaxScoreOfMostImportantLabel = p.value.labels[l];
+                }
+              });
+              return mostImportantLabel.toString(); //p.key;
+            })
+
+            //Information on hover
+            .renderTitle(true)
+            .title(function(p) {
+              var formattedTime = p.key[1].getDay() + '/' + p.key[1].getMonth() + '/' + p.key[1].getFullYear();
+
+              //Get the events
+              var events = Object.keys(p.value.events);
+              var eventString = '';
+              events.forEach(function(e) {
+                eventString += p.value.events[e] + ' : ' + e.toString() + '\n';
+              });
+
+              //Get the actors
+              var actors = Object.keys(p.value.actors);
+              var actorString = '';
+              actors.forEach(function(a) {
+                actorString += p.value.actors[a] + ' : ' + a.toString() + '\n';
+              });
+
+              //List all individual labels and their climax scores
+              var labels = Object.keys(p.value.labels);
+              var labelString = '';
+              labels.forEach(function(l) {
+                labelString += p.value.labels[l] + ' : ' + l.toString() + '\n';
+              });
+
+              var titleString =
+                                '\n-----Labels-----\n' +
+                                labelString +
+                                '\n-----Actors-----\n' +
+                                actorString +
+                                '\n-----Time-------\n' +
+                                formattedTime +
+                                '\n-----Group------\n' +
+                                p.key[0];
+              return titleString;
+            });
+
+          //A hack to make the customBubbleChart filter out 0-value bubbles while determining the x-axis range
+          dc.override(subwayChart, 'xAxisMin', function() {
+            var min = d3.min(subwayChart.data(), function (e) {
+              if (subwayChart.radiusValueAccessor()(e) > 0) {
+                return subwayChart.keyAccessor()(e);
+              }
+            });
+            return dc.utils.subtract(min, subwayChart.xAxisPadding());
+          });
+
+          dc.override(subwayChart, 'xAxisMax', function() {
+            var max = d3.max(subwayChart.data(), function (e) {
+              if (subwayChart.radiusValueAccessor()(e) > 0) {
+                return subwayChart.keyAccessor()(e);
+              }
+            });
+            return dc.utils.add(max, subwayChart.xAxisPadding());
+          });
+
+          //A hack to make the bubbleChart accept ordinal values on the y Axis
+          dc.override(subwayChart, '_prepareYAxis', function(g) {
+            this.__prepareYAxis(g);
+            this.y().rangeBands([this.yAxisHeight(), 0], 0, 1);
+          });
+
+          subwayChart.render();
+
+
+
+
+
+
+
+
+
+
+
+
+          //The row Chart which shows how important the groups are in terms of climax scores
           var groupRowChart = dc.rowChart('#rowchart_groups');
 
           //A dimension which consists of all the different group numbers
@@ -473,8 +717,7 @@
                 labelString += p.value.labels[l] + ' : ' + l.toString() + '\n';
               });
 
-              var titleString = '\n-----Events-----\n' +
-                                eventString +
+              var titleString =
                                 '\n-----Labels-----\n' +
                                 labelString +
                                 '\n-----Actors-----\n' +
