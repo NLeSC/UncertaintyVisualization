62271b1a99d7a7b3362417569bf5aba78b95cd99
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2016-02-16 13:35:23 +0100

    Add loading of data
    Path to data is hard coded. This should be changed.

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,26 @@
 from flask import Flask, jsonify
+import pandas as pd
+import codecs
+import os
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return jsonify({'message': 'hello'})
+    return jsonify({'#records': data.shape[0]})
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    print('Loading data')
+    data_dir = '/home/jvdzwaan/data/enron_data_date/enron_email_clean_json'
+
+    dfs = []
+    for doc in os.listdir(data_dir):
+        json_file = os.path.join(data_dir, doc)
+        with codecs.open(json_file, 'rb', encoding='utf-8') as f:
+            dfs.append(pd.read_json(f))
+    data = pd.concat(dfs)
+
+    print('Finished loading data')
+
+    app.run(debug=False)
