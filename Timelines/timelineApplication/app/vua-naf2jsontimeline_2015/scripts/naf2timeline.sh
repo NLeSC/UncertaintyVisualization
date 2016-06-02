7769c6914125ef86bb57cf8590a5307e9a17adfc
Maarten van Meersbergen <Maarten van Meersbergen>
2016-06-02 17:14:51 +0200

    removed Timelines dir as it was unnecessary

--- Timelines/timelineApplication/app/vua-naf2jsontimeline_2015/scripts/naf2timeline.sh
+++
@@ -1,5 +0,0 @@
-java -Xmx2000m -cp ../lib/EventCoreference-1.0-SNAPSHOT-jar-with-dependencies.jar eu.newsreader.eventcoreference.naf.ClusterEventObjects --microstories 4 --naf-folder "../tmp" --event-folder "../tmp" --extension ".naf" --project timeline --communication-frames "../resources/communication.txt" --grammatical-frames "../resources/grammatical.txt" --contextual-frames "../resources/contextual.txt" --bridging --frame-relations ../resources/frRelation.xml --frame-level 3
-
-java -Xmx2000m -cp ../lib/EventCoreference-1.0-SNAPSHOT-jar-with-dependencies.jar eu.newsreader.eventcoreference.naf.MatchEventObjects --match-type ILILEMMA --event-folder "../tmp/events/contextual" --single-output --event-type "contextual" --ili-uri
-
-java -Xmx2000m -cp ../lib/EventCoreference-1.0-SNAPSHOT-jar-with-dependencies.jar eu.newsreader.eventcoreference.input.TrigToJsonTimeLine --trig-folder "../tmp/events/contextual" --ili "../resources/wn3-ili-synonyms.txt"
