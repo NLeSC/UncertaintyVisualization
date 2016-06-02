310708a00f26323e34bd6b2ad720324d420d5aae
Maarten van Meersbergen <Maarten van Meersbergen>
2016-06-02 14:08:52 +0200

    rename attempt 2.1

--- app/scripts/viewperspectives/view.perspectives.controller.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  'use strict';
-
-  function ViewPerspectivesController() {
-  }
-
-  angular.module('uncertApp.viewperspectives')
-    .controller('ViewPerspectivesController', ViewPerspectivesController);
-})();
